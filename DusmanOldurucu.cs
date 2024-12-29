f3a52c8fe526f72454866d04beffdce75f80a33d
Talha <83809076+cllarr@users.noreply.github.com>
2024-12-29 11:32:14 +0300

    The "DusmanOldurucu" script was added.

---
+++ DusmanOldurucu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DusmanOldurucu : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Dusman"))
+        {
+            Destroy(collision.gameObject); //optimizasyon için
+        }
+    }
+}
